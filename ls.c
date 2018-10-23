9f289ca9039cb2bb89677fd73f12d1fe4a1476c7
Anuradha Weeraman <aweeraman@gmail.com>
2018-10-23 23:52:31 +0530

    Added ls

---
+++ ls.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<dirent.h>
+
+int main(int argc, char **argv) {
+  DIR           *dirp;
+  struct dirent *dire;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: ls directory_name\n");
+    exit(1);
+  }
+
+  if ((dirp = opendir(argv[1])) == NULL) {
+    fprintf(stderr, "Can't open %s\n", argv[1]);
+    exit(2);
+  }
+
+  while ((dire = readdir(dirp)) != NULL) {
+    printf("%s\n", dire->d_name);
+  }
+}
